b09f64b6cd192233b1d8bca239109690f48dd71f
Bushra123445 <65974219+Bushra123445@users.noreply.github.com>
2026-08-21 11:42:55 +0500

    Update LoginPage.ts

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -1,10 +1,13 @@
-import { Page, Locator } from '@playwright/test';
+
+import { Page, Locator, expect } from '@playwright/test';
 
 export class LoginPage {
   readonly page: Page;
   readonly username: Locator;
   readonly password: Locator;
   readonly loginButton: Locator;
+  readonly togglePassword: Locator;
+  readonly message: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -12,15 +15,29 @@ export class LoginPage {
     this.username = page.locator('#username');
     this.password = page.locator('#password');
     this.loginButton = page.locator('#loginButton');
+    this.togglePassword = page.locator('#togglePassword');
+    this.message = page.locator('#message');
   }
 
   async goto() {
-    await this.page.goto('/app/login.html');
+    await this.page.goto('login.html', {
+      waitUntil: 'domcontentloaded',
+    });
+
+    await expect(this.username).toBeVisible({
+      timeout: 15000,
+    });
   }
 
   async login(username: string, password: string) {
     await this.username.fill(username);
     await this.password.fill(password);
     await this.loginButton.click();
+  }
+
+  async verifyLoginPage() {
+    await expect(this.username).toBeVisible();
+    await expect(this.password).toBeVisible();
+    await expect(this.loginButton).toBeVisible();
   }
 }
